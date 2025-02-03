d16b13d6d8311cd166d3a5e84de4df4f936f76bb
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-02-03 16:26:16 +0200

    Get the Data from the user when autehenticated to the Finalize Booking action

--- RealState.Presentation/Controllers/BookingController.cs
+++ RealState.Presentation/Controllers/BookingController.cs
@@ -1,29 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using RealState.Domain.Entities;
+using RealState.Domain.Entities.Identity;
 using RealState.Domain.Services.Contract;
+using System.Security.Claims;
 
 namespace RealState.Presentation.Controllers
 {
     public class BookingController : Controller
     {
         private readonly IVillaService _villaService;
+        private readonly UserManager<AppUser> _userManager;
 
-        public BookingController(IVillaService villaService)
+        public BookingController(IVillaService villaService,
+            UserManager<AppUser> userManager)
         {
             _villaService = villaService;
+            _userManager = userManager;
         }
 
+        [Authorize]
         public async Task<IActionResult> FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
         {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            var user = await _userManager.FindByEmailAsync(userEmail);
+
             Booking booking = new Booking() 
             {
                 VillaId = villaId,
                 Villa = await _villaService.GetVillaWithAmenityById(villaId),
                 CheckInDate = checkInDate,
                 NumberOfNights = nights,
-                CheckOutDate = checkInDate.AddDays(nights)
+                CheckOutDate = checkInDate.AddDays(nights),
+                BookingEmail = userEmail,
+                PhoneNumber = user.PhoneNumber,
+                BookingName = user.Name,
             };
-
+            booking.TotalCost =  (double) booking.Villa.Price * nights;
             return View(booking);
         }
     }
